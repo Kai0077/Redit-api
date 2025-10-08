4a5cdef0012689cb11368ccab5de0697459f47b3
KaiSej007 <kailyubo@gmail.com>
2025-10-08 15:29:38 +0200

    connection is established

--- Redit-api/Program.cs
+++ Redit-api/Program.cs
@@ -4,39 +4,31 @@ using Redit_api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Env.Load();
+// Load your env file (rename or pass the filename explicitly)
+Env.Load("Secret.env");
 
+// Build connection string from env
+string host = Environment.GetEnvironmentVariable("DB_HOST")!;
+string port = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+string database = Environment.GetEnvironmentVariable("DB_NAME")!;
+string user = Environment.GetEnvironmentVariable("DB_USER")!;
+string password = Environment.GetEnvironmentVariable("DB_PASSWORD")!;
 
-// Build connection string from environment variables
-var host = Environment.GetEnvironmentVariable("DB_HOST");
-var port = Environment.GetEnvironmentVariable("DB_PORT");
-var database = Environment.GetEnvironmentVariable("DB_NAME");
-var user = Environment.GetEnvironmentVariable("DB_USER");
-var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+string connectionString =
+    $"Host={host};Port={port};Database={database};Username={user};Password={password};Ssl Mode=Disable";
 
-
-var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password};Ssl Mode=Disable";
-
-// Register EF Core
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
 builder.Services.AddControllers();
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.MapControllers();
 app.Run();
